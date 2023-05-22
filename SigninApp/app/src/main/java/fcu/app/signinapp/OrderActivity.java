0f1a090307b14cabc1f0468c7ffa51a94fe6df8a
Vanessa1128 <weiweimpnkey70113@gmail.com>
2023-05-22 14:11:17 +0800

    重新命名
    order.java->OrderActivity.java

--- SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
+++ SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class order extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
