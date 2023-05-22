23af6c56a83cb672975f2dfb486d562a271c5145
Vanessa1128 <weiweimpnkey70113@gmail.com>
2023-05-22 16:02:01 +0800

    把TabLayout換成按鈕
    把TabLayout換成按鈕

--- SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
+++ SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
@@ -3,12 +3,25 @@ package fcu.app.signinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private Switch Switch_Favorite;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+
     }
+
+
+
+
 }
