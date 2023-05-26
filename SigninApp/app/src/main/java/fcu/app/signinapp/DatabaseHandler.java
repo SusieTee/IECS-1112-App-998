3fbe24ad47bf5b61a579e5d54995ff87ef7f011c
SusieTee <t.h.s.98.hotmail.com@gmail.com>
2023-05-27 02:12:20 +0800

    加入Database

---
+++ SigninApp/app/src/main/java/fcu/app/signinapp/DatabaseHandler.java
@@ -0,0 +1,53 @@
+package fcu.app.signinapp;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DatabaseHandler {
+
+    private AppCompatActivity activity;
+
+    private SQLiteDatabase database;
+
+    private static final String DATABASE_NAME = "fcu_breakfast.db";
+
+    private static final String CREATE_MEAL_TABLE = "CREATE TABLE IF NOT EXISTS Meals (" +
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "name TEXT NOT NULL, " +
+            "description TEXT, " +
+            "price INTEGER NOT NULL)";
+
+    public DatabaseHandler(AppCompatActivity activity){
+        this.activity = activity;
+    }
+
+    public void open(){
+        database = activity.openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+        database.execSQL(CREATE_MEAL_TABLE);
+
+    }
+
+    public void addMeal(String name, String description, int price) {
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("description", description);
+        values.put("price", price);
+        database.insert("Meals", null, values);
+    }
+
+    public Cursor getAllMeals() {
+        Cursor cursor = database.rawQuery("SELECT * FROM Meals", null);
+        Toast.makeText(activity, cursor.getCount() + "is added", Toast.LENGTH_SHORT).show();
+
+        return cursor;
+    }
+
+
+}
