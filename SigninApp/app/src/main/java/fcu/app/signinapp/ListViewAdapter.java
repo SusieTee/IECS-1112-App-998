a3282daf3e04a3eae23edb256cef6aa6d25d3d92
Vanessa1128 <weiweimpnkey70113@gmail.com>
2023-05-29 13:06:08 +0800

    ListView實作中途
    0327到02:13:48的內容實作

--- SigninApp/app/src/main/java/fcu/app/signinapp/ListViewAdapter.java
+++ SigninApp/app/src/main/java/fcu/app/signinapp/ListViewAdapter.java
@@ -1,4 +1,51 @@
 package fcu.app.signinapp;
 
-public class ListViewAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<FoodItem> listFoods;
+
+    public ListViewAdapter(Context context, List<FoodItem> listFoods) {
+        this.context = context;
+        this.listFoods = listFoods;
+    }
+
+    @Override
+    public int getCount() {
+        return listFoods.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.food_item_layout,viewGroup,false);
+        }
+
+        FoodItem food = listFoods.get(i);
+
+        ImageView iv = view.findViewById(R.id.iv_food_image1);
+        iv.setImageResource(food.getImageID());
+
+        return iv;
+    }
 }
--- SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
+++ SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
@@ -3,20 +3,26 @@ package fcu.app.signinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Switch;
+import android.widget.ListView;
 
-import com.google.android.material.tabs.TabLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderActivity extends AppCompatActivity {
 
-    private Switch Switch_Favorite;
+    //private Switch Switch_Favorite;
 
+    private ListView lvFoods;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        lvFoods = findViewById(R.id.lv_foods);
+
+        List<FoodItem> lsFoods = new ArrayList<FoodItem>();
+        lsFoods.add(new FoodItem(R.drawable.點餐預覽,"1號餐",110));
 
 
     }
