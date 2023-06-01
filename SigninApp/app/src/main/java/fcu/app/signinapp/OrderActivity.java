ac121e324e489acb8b0ef4939afa283eaa8d3e1e
Vanessa1128 <weiweimpnkey70113@gmail.com>
2023-06-01 22:30:20 +0800

    新增餐點資料
    新增餐點資料

--- SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
+++ SigninApp/app/src/main/java/fcu/app/signinapp/OrderActivity.java
@@ -13,6 +13,8 @@ public class OrderActivity extends AppCompatActivity {
     //private Switch Switch_Favorite;
 
     private ListView lvFoods;
+    private ListView lvFoodsSingle;
+    private ListView lvFoodsDrinks;
 
 
     @Override
@@ -20,6 +22,8 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         lvFoods = findViewById(R.id.lv_foods);
+        lvFoodsSingle = findViewById(R.id.lv_foods_single);
+        lvFoodsDrinks = findViewById(R.id.lv_foods_drinks);
 
         List<FoodItem> lsFoods = new ArrayList<FoodItem>();
         lsFoods.add(new FoodItem(R.drawable.preview,"1號餐",110));
@@ -28,6 +32,25 @@ public class OrderActivity extends AppCompatActivity {
 
         ListViewAdapter adapter = new ListViewAdapter(this,lsFoods);
         lvFoods.setAdapter(adapter);
+
+
+        List<FoodItem> lsFoodsSingle = new ArrayList<FoodItem>();
+        lsFoodsSingle.add(new FoodItem(R.drawable.preview,"蛋餅",20));
+        lsFoodsSingle.add(new FoodItem(R.drawable.preview,"三明治",25));
+        lsFoodsSingle.add(new FoodItem(R.drawable.preview,"炒麵",45));
+
+        ListViewAdapter adapter_single = new ListViewAdapter(this,lsFoodsSingle);
+        lvFoodsSingle.setAdapter(adapter_single);
+
+
+
+        List<FoodItem> lsFoodsDrinks = new ArrayList<FoodItem>();
+        lsFoodsDrinks.add(new FoodItem(R.drawable.preview,"奶茶",45));
+        lsFoodsDrinks.add(new FoodItem(R.drawable.preview,"紅茶",25));
+        lsFoodsDrinks.add(new FoodItem(R.drawable.preview,"柳橙汁",35));
+
+        ListViewAdapter adapter_drinks = new ListViewAdapter(this,lsFoodsDrinks);
+        lvFoodsDrinks.setAdapter(adapter_drinks);
 
     }
 
